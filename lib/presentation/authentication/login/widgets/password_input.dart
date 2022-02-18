67ec8288dafe49eb9c047489b2116fe73748a71b
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-02-18 22:24:51 +0700

    Update `password_input`

--- lib/presentation/authentication/login/widgets/password_input.dart
+++ lib/presentation/authentication/login/widgets/password_input.dart
@@ -1,17 +1,39 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class PasswordInput extends StatelessWidget {
+class PasswordInput extends StatefulWidget {
   const PasswordInput({Key? key}) : super(key: key);
+
+  @override
+  State<PasswordInput> createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      obscureText: true,
-      decoration: const InputDecoration(
-          labelText: "Password",
-          hintText: "Enter your password",
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          border: OutlineInputBorder()),
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Enter your password',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        border: const OutlineInputBorder(),
+        // filled: true,
+        suffixIcon: GestureDetector(
+          dragStartBehavior: DragStartBehavior.down,
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: Icon(
+            _obscureText ? Icons.visibility : Icons.visibility_off,
+            semanticLabel: _obscureText ? 'Show password' : 'Hide password',
+          ),
+        ),
+      ),
     );
   }
 }
