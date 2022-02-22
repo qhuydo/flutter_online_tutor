2e6d885976f93fd20e953a8cf5f7df615e8501a3
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-02-22 22:33:54 +0700

    Update `topic_card` & `topic_list`

--- lib/presentation/tutor/widgets/topic_list.dart
+++ lib/presentation/tutor/widgets/topic_list.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../../common.dart';
@@ -6,13 +8,24 @@ import 'topic_chip.dart';
 class TopicList extends StatelessWidget {
   const TopicList({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Wrap(
-          spacing: 8,
-          runSpacing: 0,
+  Widget buildListAtLinuxPlatform(BuildContext context) {
+    return Wrap(
+      spacing: 8,
+      runSpacing: 4,
+      children: const [
+        TopicChip(),
+        TopicChip(),
+        TopicChip(),
+        TopicChip(),
+      ],
+    );
+  }
+
+  Widget buildList2(BuildContext context) {
+    return Stack(children: [
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
           children: const [
             TopicChip(),
             TopicChip(),
@@ -20,7 +33,53 @@ class TopicList extends StatelessWidget {
             TopicChip(),
           ],
         ),
-      ],
+      ),
+      Positioned(
+        // bottom: 0,
+        // left: 0,
+        right: 0,
+        width: 20,
+        height: 60,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.topRight,
+              stops: const [0.0, 1.0],
+              colors: [
+                Theme.of(context).cardColor.withOpacity(0.0),
+                Theme.of(context).cardColor,
+              ],
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Widget buildList(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: ListView.separated(
+        // primary: false,
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(width: 8);
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return const TopicChip();
+        },
+        itemCount: 4,
+      ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isLinux
+        ? buildListAtLinuxPlatform(context)
+        : buildList(context);
   }
 }
