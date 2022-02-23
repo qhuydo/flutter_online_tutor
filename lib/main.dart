3e090621add8371fcb480ed89ffdd5872e031bb3
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-02-23 17:40:12 +0700

    Set minimum window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:window_size/window_size.dart';
 
 import 'presentation/routes/app_routes.gr.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+    setWindowMinSize(const Size(600, 750));
+  }
   runApp(OnlySenpaiApp());
 }
 
