8ffbb591e472ec5a133047ec91e078c3da712119
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-03-05 15:56:32 +0700

    Filter specialities at `tutor_page` & update Vietnamese translation

--- lib/presentation/course_ebook/course_list/course_tab_page.dart
+++ lib/presentation/course_ebook/course_list/course_tab_page.dart
@@ -1,11 +1,9 @@
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
-import '../../../utils/constants.dart';
 import '../../common.dart';
 import '../../common/widgets/scaffold_with_search_bar.dart';
 import '../../common/widgets/search_item_row_placeholder.dart';
 import 'widgets/course_list_card.dart';
-import 'widgets/specialities_filter_row.dart';
 
 class CourseTabPage extends StatelessWidget {
   const CourseTabPage({Key? key}) : super(key: key);
@@ -35,34 +33,22 @@ class CourseTabPage extends StatelessWidget {
       actions: actions,
       leadingActions: leadingActions,
       body: FloatingSearchBarScrollNotifier(
-        child: SingleChildScrollView(
+        child: GridView.builder(
           primary: false,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: searchBarHeight + 12,
-              left: smallItemSpacing,
-              right: smallItemSpacing,
-            ),
-            child: Column(
-              children: [
-                const SizedBox(child: SpecialitiesFilterRow(), height: 40,),
-                GridView.builder(
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  // separatorBuilder: (context, index) => const SizedBox(height: 8,),
-                  itemBuilder: (context, index) {
-                    return const CourseListCard();
-                  },
-                  // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  //   childAspectRatio: 1/1.25,
-                  //   crossAxisCount: 2,
-                  // ),
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 360, childAspectRatio: 1 / 1.25),
-                ),
-              ],
-            ),
+          shrinkWrap: true,
+          padding: const EdgeInsets.only(top: searchBarHeight + 12),
+          itemCount: 5,
+          // separatorBuilder: (context, index) => const SizedBox(height: 8,),
+          itemBuilder: (context, index) {
+            return const CourseListCard();
+          },
+          // gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          //   childAspectRatio: 1/1.25,
+          //   crossAxisCount: 2,
+          // ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 1/1.25
           ),
         ),
       ),
--- lib/presentation/tutor/list/tutor_page.dart
+++ lib/presentation/tutor/list/tutor_page.dart
@@ -1,9 +1,11 @@
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
+import '../../../utils/constants.dart';
 import '../../common.dart';
 import '../../common/widgets/scaffold_with_search_bar.dart';
 import '../../common/widgets/search_item_row_placeholder.dart';
 import '../list/widgets/tutor_card.dart';
+import 'widgets/specialities_filter_row.dart';
 
 class TutorPage extends StatelessWidget {
   const TutorPage({Key? key}) : super(key: key);
@@ -33,13 +35,29 @@ class TutorPage extends StatelessWidget {
       actions: actions,
       leadingActions: leadingActions,
       body: FloatingSearchBarScrollNotifier(
-        child: ListView.builder(
-          padding: const EdgeInsets.only(top: searchBarHeight),
-          itemCount: 5,
-          // separatorBuilder: (context, index) => const Divider(),
-          itemBuilder: (context, index) {
-            return const TutorCard();
-          },
+        child: SingleChildScrollView(
+          primary: false,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: searchBarHeight + 12,
+              left: smallItemSpacing,
+              right: smallItemSpacing,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(child: SpecialitiesFilterRow(), height: 40,),
+                ListView.builder(
+                  primary: false,
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  // separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) {
+                    return const TutorCard();
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
