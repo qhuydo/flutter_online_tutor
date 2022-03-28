0f1f389551031b802061b4d939e1bb89bb1e0624
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-03-28 23:58:47 +0700

    Update `country_form_dropdown` & change default window size on linux

--- lib/presentation/user/profile/widgets/country_form_dropdown.dart
+++ lib/presentation/user/profile/widgets/country_form_dropdown.dart
@@ -1,63 +1,61 @@
+import 'package:flutter/cupertino.dart';
 import 'package:twemoji/twemoji.dart';
 
+import '../../../../domain/common/constants/countries.dart';
+import '../../../../domain/common/models/country.dart';
 import '../../../common.dart';
 import '../../../common/utils/string_utils.dart';
 
 class CountryFormDropdown extends StatelessWidget {
   final bool showLeadingIcon;
+  final Country? value;
+  final ValueChanged? onChanged;
 
   const CountryFormDropdown({
     Key? key,
     this.showLeadingIcon = true,
+    this.value,
+    this.onChanged,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField<String?>(
-      items: <String?>[
-        null,
-        'VN',
-        'GB',
-        'UA',
-        'GE',
-        'EE',
-        'LV',
-        'LT',
-      ].map<DropdownMenuItem<String?>>((String? value) {
-        return value != null
-            ? DropdownMenuItem<String>(
-                value: value,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Twemoji(
-                      emoji: value.toCountryFlagFromCountryCode(),
-                      width: 32,
-                      height: 32,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      value,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+    return DropdownButtonFormField<Country>(
+      isDense: false,
+      isExpanded: true,
+      items: countryMap.values.map<DropdownMenuItem<Country>>((Country value) {
+        return DropdownMenuItem<Country>(
+          value: value,
+          child: FittedBox(
+            child: Row(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Twemoji(
+                  emoji: value.isoCode.toCountryFlagFromCountryCode(),
+                  width: 32,
+                  height: 32,
                 ),
-              )
-            : DropdownMenuItem<String?>(
-                child: Container(),
-              );
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  value.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        );
       }).toList(),
-      value: null,
+      value: value,
       decoration: InputDecoration(
         // contentPadding: EdgeInsets.all(18),
         border: const OutlineInputBorder(),
         icon: showLeadingIcon ? const Icon(Icons.language) : null,
         labelText: AppLocalizations.of(context)!.countryDropdownLabel,
       ),
-      onChanged: (String? value) {},
+      onChanged: onChanged ?? (_) {},
     );
   }
 }
