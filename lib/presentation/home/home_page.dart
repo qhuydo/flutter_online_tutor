5c113d5f7a66113da95c82f3db6a43edd89eda70
qhuydo <46428103+qhuydo@users.noreply.github.com>
2022-03-29 22:55:15 +0700

    Update `home_page`: change `SalomonBottomBar` > `NavigationBar`

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
-import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 import '../common.dart';
 import '../common/routes/app_routes.gr.dart';
@@ -9,36 +6,6 @@ import '../common/utils/default_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  SalomonBottomBarItem buildNavBarItem({
-    required IconData icon,
-    IconData? activeIcon,
-    required String text,
-    required Color selectedColour,
-  }) {
-    final isDesktop =
-        Platform.isLinux || Platform.isMacOS || Platform.isWindows;
-
-    return SalomonBottomBarItem(
-      activeIcon: activeIcon != null
-          ? Icon(
-              activeIcon,
-              size: isDesktop ? 32 : null,
-            )
-          : null,
-      icon: Icon(
-        icon,
-        size: isDesktop ? 32 : null,
-      ),
-      title: Text(
-        text,
-        // style: const TextStyle(
-        //   fontSize: 16,
-        // ),
-      ),
-      selectedColor: selectedColour,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,44 +25,41 @@ class HomePage extends StatelessWidget {
         );
       },
       bottomNavigationBuilder: (BuildContext context, tabsRouter) {
-        return SalomonBottomBar(
-          currentIndex: tabsRouter.activeIndex,
-          onTap: tabsRouter.setActiveIndex,
-          itemPadding: const EdgeInsets.symmetric(
-            vertical: 12,
-            horizontal: 12,
-          ),
-          margin: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 8,
-          ),
-          items: [
-            buildNavBarItem(
-              icon: Icons.home_outlined,
-              activeIcon: Icons.home,
-              text: context.l10n.homeBottomNavItem,
-              selectedColour: Colors.blue,
+        return NavigationBar(
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          selectedIndex: tabsRouter.activeIndex,
+          onDestinationSelected: tabsRouter.setActiveIndex,
+          destinations: [
+            NavigationDestination(
+              icon: Icon(tabsRouter.activeIndex == 0
+                  ? Icons.home
+                  : Icons.home_outlined),
+              label: context.l10n.homeBottomNavItem,
             ),
-            buildNavBarItem(
-                icon: Icons.people_outlined,
-                activeIcon: Icons.people,
-                text: context.l10n.tutorBottomNavBarItem,
-                selectedColour: Colors.teal),
-            buildNavBarItem(
-                icon: Icons.book_outlined,
-                activeIcon: Icons.book,
-                text: context.l10n.courseBottomNavItem,
-                selectedColour: Colors.green),
-            buildNavBarItem(
-                icon: Icons.event_outlined,
-                activeIcon: Icons.event,
-                text: context.l10n.scheduleBottomNavItem,
-                selectedColour: Colors.pinkAccent),
-            buildNavBarItem(
-                icon: Icons.message_outlined,
-                activeIcon: Icons.message,
-                text: context.l10n.messagesBottomNavItem,
-                selectedColour: Colors.deepOrange),
+            NavigationDestination(
+              icon: Icon(
+                tabsRouter.activeIndex == 1
+                    ? Icons.people
+                    : Icons.people_outlined,
+              ),
+              label: context.l10n.tutorBottomNavBarItem,
+            ),
+            NavigationDestination(
+              icon: Icon(
+                tabsRouter.activeIndex == 2
+                    ? Icons.event_outlined
+                    : Icons.all_inbox_outlined,
+              ),
+              label: context.l10n.scheduleBottomNavItem,
+            ),
+            NavigationDestination(
+              icon: Icon(
+                tabsRouter.activeIndex == 3
+                    ? Icons.message
+                    : Icons.message_outlined,
+              ),
+              label: context.l10n.messagesBottomNavItem,
+            ),
           ],
         );
       },
